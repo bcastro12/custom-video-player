561a65b200263dfc343777915ec134c69edccb18
bcastro12 <bcastroduarte12@gmail.com>
2020-08-08 18:15:31 -0300

    updated

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ window.onload = () => {
     vid.addEventListener("play", () => (btnplaypause.className = "fa fa-pause"));
 
     // video duration / progress bar / tooltip
-    vid.addEventListener("loadedmetadata", () => {
+    vid.addEventListener("loadedmetadata", (event) => {
         document.getElementById("total-time").innerHTML = " / " + convertTime(parseInt(vid.duration));
 
         vidTime = setInterval(function () {
